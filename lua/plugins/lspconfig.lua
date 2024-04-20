96a661b66afa51e56b5ed5c707d21877e25a7d58
Wish <70953480+wish6424@users.noreply.github.com>
2024-04-20 21:59:48 +0700

    try to fix lsp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -16,8 +16,15 @@ return {
             ignore = { "*" },
           },
         },
+        ruff_lsp = {
+          on_attach = function(client, bufnr)
+            if client.name == "ruff_lsp" then
+              -- Disable hover in favor of Pyright
+              client.server_capabilities.hoverProvider = false
+            end
+          end,
+        },
       },
     },
   },
 }
--- return {}
